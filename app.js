febef0b9562a19bb6e29aaccf54b5490d98b3b2f
minTwin <michaelmin2020@gmail.com>
2022-01-03 10:59:24 -0600

    Initial Commit
    Prototype Weather API project for gaining more contextual understand with building API with JavaScript and NPM modules.

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");    
+});
+
+app.post("/", function(req, res){
+
+    const query= req.body.cityName;
+    const apiKey = "e5bc96427df08e0c71525599084dcd34";
+    const unit = "imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const feels_like = weatherData.main.feels_like;
+            const description = weatherData.weather[0].main;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+            console.log(temp)
+            console.log(feels_like);
+            console.log(description);
+            console.log(icon);
+            console.log(imageURL);
+
+            res.write("<p>Current Sky Condition: " + description +"</p>");
+            res.write("<h1>The temperture in " + query + " is " + temp + " degrees fahrenheit</h1>");
+            res.write('<head><meta charset="utf-8"></head>');
+            res.write("<img width=200 src=" + imageURL + ">");
+
+            res.send();
+        })
+    });
+})
+
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+})
